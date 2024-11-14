d3f9c47afc24f78e6f82999371f767ac82064d23
birukGetachew <burab1742@gmail.com>
2024-11-14 13:00:30 -0800

    hellow

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -8,7 +8,7 @@ function NavBar() {
   };
 
   return (
-    <nav className="pl-28 pr-28 flex items-center justify-between bg-opacity-10 bg-black w-screen overflow-x-hidden z-11 top-0 left-0 text-white h-[10vh] box-border">
+    <nav className="px-4 sm:px-8 md:px-16 lg:px-28 flex items-center justify-between bg-opacity-10 bg-black w-screen overflow-x-hidden z-11 top-0 left-0 text-white h-[10vh] box-border">
       <div className="">
         <img src="./logo.png" className="w-20 h-15" style={{ borderRadius: '30px' }} alt="Logo" />
       </div>
@@ -33,32 +33,32 @@ function NavBar() {
 
       {/* Navigation Menu */}
       <ul className={`flex space-x-6 md:flex ${isOpen ? 'flex-col items-center py-40 gap-10 absolute bg-black z-50 bg-opacity-95 top-16 left-0 w-full h-screen' : 'hidden md:flex'}`}>
-        <li className="cursor-pointer">
+        <li className="cursor-pointer overflow-hidden h-auto">
           <a href="/" className="hover:text-white text-yellow-500 font-poppins">
             Home
           </a>
         </li>
-        <li className="cursor-pointer">
+        <li className="cursor-pointer overflow-hidden h-auto">
           <a href="/about" className="hover:text-white text-yellow-500 font-poppins">
             About
           </a>
         </li>
-        <li className="cursor-pointer">
+        <li className="cursor-pointer overflow-hidden h-auto">
           <a href="/services" className="hover:text-white text-yellow-500 font-poppins">
             Products and services
           </a>
         </li>
-        <li className="cursor-pointer">
+        <li className="cursor-pointer overflow-hidden h-auto">
           <a href="/shop" className="hover:text-white text-yellow-500 font-poppins">
             Shop
           </a>
         </li>
-        <li className="cursor-pointer">
+        <li className="cursor-pointer overflow-hidden h-auto">
           <a href="/gallery" className="hover:text-white text-yellow-500 font-poppins">
             Gallery
           </a>
         </li>
-        <li className="cursor-pointer z-10">
+        <li className="cursor-pointer z-10 h-30">
           <a href="/contact" className="hover:text-white text-yellow-500 font-poppins">
             Contact us
           </a>
