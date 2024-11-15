07faf5ef4cacd8f024dc544a239ebfb7f250f565
birukGetachew <burab1742@gmail.com>
2024-11-14 23:06:13 -0800

    hellow

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -13,49 +13,26 @@ function NavBar() {
         <img src="./logo.png" className="w-20 h-15" style={{ borderRadius: '30px' }} alt="Logo" />
       </div>
 
-      {/* Hamburger Menu Icon or "X" */}
+      {/* Hamburger Menu Icon */}
       <div className="md:hidden cursor-pointer" onClick={toggleMenu}>
-        {isOpen ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="w-8 h-8 text-yellow-500"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="w-8 h-8 text-yellow-500"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        )}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          className="w-8 h-8 text-yellow-500"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
       </div>
 
       {/* Navigation Menu */}
-      <ul
-        className={`flex space-x-6 py-20 h-[90vh] md:flex ${
-          isOpen
-            ? 'flex-col items-center gap-10 absolute bg-black z-50 bg-opacity-95 top-16 left-0 w-full h-screen'
-            : 'hidden md:flex'
-        }`}
-      >
+      <ul className={`flex space-x-6 md:flex ${isOpen ? 'flex-col items-center gap-10 absolute bg-black z-50 bg-opacity-95 top-16 left-0 w-full h-screen' : 'hidden md:flex'}`}>
         <li className="cursor-pointer overflow-hidden h-auto">
           <a href="/" className="hover:text-white text-yellow-500 font-poppins">
             Home
